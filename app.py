6fb0c3c428c8a8dfe4fe1daa0916040400676729
Marcel <marcelgiglio@users.noreply.github.com>
2019-11-27 19:42:34 -0300

    attempt to reduce processing #1
    this try to reduce processing by validate the number before it goes on recursive calculus.
    But it take a lot of time and still didn't advance until 11th.
    maybe we should try to create less numbers, instead of validate each one.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 def recursiveCalc(n, counter=0, steps=''):
     if n<10:
         steps += 'Total Steps: ' + str(counter) + '.'
@@ -19,12 +20,31 @@ def calc(n):
 def printNumber(n, steps):
     print (str(n) + ': ' + steps)
 
-max = -1
+def notCrescentOrder(n):
+    return "".join(sorted(str(n))) != str(n)
+
+def HaveForbiddenPair(n):
+    forbidenPairs = [('2','2'), ('2','3'), ('2','4'), ('3','3')]
+    ans = False
+    n = str(n)
+    for pair in forbidenPairs:
+        if (pair[0] in n and pair[1] in n.replace(pair[0],'',1)):
+                ans = True
+    return ans
+
+def validNumber(n):
+    ans = True
+    if (notCrescentOrder(n) or HaveForbiddenPair(n)):
+        ans = False
+    return ans
+
+max = 0
 bestNumbers = []
 for i in range(1000000):
-    result = recursiveCalc(i)
-    if(result[1] > max):
-        max = result[1]
-        printNumber(i, result[0])
-        bestNumbers.append(i)
+    if (validNumber(i)):
+        result = recursiveCalc(i)
+        if(result[1] > max):
+            max = result[1]
+            printNumber(i, result[0])
+            bestNumbers.append(i)
 print (bestNumbers)
